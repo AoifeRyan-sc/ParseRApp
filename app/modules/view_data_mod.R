72b3eecddd0fd9664277a2121e1837592730e791
AoifeRyan-sc <125266574+AoifeRyan-sc@users.noreply.github.com>
2025-03-31 17:08:39 +0100

    make link_click work

--- app/modules/view_data_mod.R
+++ app/modules/view_data_mod.R
@@ -3,7 +3,16 @@ dataDisplayUi <- function(id){
   bslib::card(
     bslib::card_header("Uploaded Data"),
     bslib::card_body(
-      shiny::uiOutput(ns("data_view_dropdown")),
+      dropdownButton_with_tooltip(
+        select_input_with_tooltip(id = ns("url_column"), title = "URL Column",
+                                  icon_info = "The name of the column with the post url"),
+        select_input_with_tooltip(id = ns("display_columns"), title = "Display Columns",
+                                  icon_info = "The columns you want to display in the Uploaded Data table",
+                                  multiple_selections = TRUE),
+        # shiny::actionButton(ns("update_table"), "Update Table"),
+        dropdown_title = "Modify Table View",
+        icon_info = "Click here for table view customisation"
+      ),
       DT::dataTableOutput(ns("data_display"))
     ),
     full_screen = TRUE,
@@ -19,62 +28,50 @@ dataDisplayServer <- function(id, r){
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
     
-    observe({
+    shiny::observe({
+      shiny::updateSelectizeInput(session = session, "url_column", choices = colnames(r$df), selected = NULL)
+      shiny::updateSelectizeInput(session = session, "display_columns", choices = colnames(r$df), selected = NULL)
+    })
+    
+    # observeEvent(input$update_table, {
+    shiny::observe({
       req(r$df)
-      if (is.null(input$display_columns)){
-        r$cols <- colnames(r$df)
+      r$cols <- if (is.null(input$display_columns)){
+        colnames(r$df)
       } else {
-        r$cols <- input$display_columns
+        input$display_columns
+      }
+      
+      r$url_var <- if (input$url_column == ""){
+        NULL
+      } else {
+        input$url_column
       }
     })
     
-    # shiny::observe({
-    #   req(r$df)
-    #   r$text_var <- input$text_column
-    #   r$display_var <- input$display_columns
-    #   r$url_var <- input$url_column
-    # }) # set reactive values
-    
-    output$data_view_dropdown <- shiny::renderUI({
-      dropdownButton_with_tooltip(
-        select_input_with_tooltip(id = ns("url_column"), title = "URL Column",
-                                  icon_info = "The name of the column with the post url",
-                                  choice_list = colnames(r$df)),
-        select_input_with_tooltip(id = ns("display_columns"), title = "Display Columns",
-                                  icon_info = "The columns you want to display in the Uploaded Data table",
-                                  choice_list = colnames(r$df), multiple_selections = TRUE),
-        dropdown_title = "Modify Table View",
-        icon_info = "Click here for table view customisation"
-      )
-    })
-    
-    # shiny::observeEvent(input$file_upload, {
-    #   req(r$df)
-    #   # shiny::updateSelectizeInput(session = session, "text_column", choices = colnames(r$df), selected = NULL)
-    #   shiny::updateSelectizeInput(session = session, "url_column", choices = colnames(r$df), selected = NULL)
-    #   shiny::updateSelectizeInput(session = session, "display_columns", choices = colnames(r$df), selected = NULL)
-    # }) # selectInput updates
-    # 
-    
     output$data_display <- DT::renderDataTable({
-      # message(is.null(r$url_var))
-      # message("url var: ", r$url_var)
-      # if (!is.null(r$url_var)){
-      #   message("link clicking")
-      #   r$df <- LimpiaR::limpiar_link_click(r$df, r$url_var)
-      # }
-      print(r$cols)
-
-      DT::datatable(
-      r$df[r$cols],
-      filter = "top",
-      # extensions = c("Buttons"),
-      # options = list(
-      #   select = list(maxOptions = 2000),
-      #   dom = 'Bfrtip',
-      #   buttons = c("copy", "csv", "excel", "pdf")
-      # )
-      )
+      message("url col: ", r$url_var)
+      if (is.null(r$url_var)){
+        print("not link clicking")
+        r$df[r$cols] %>%
+          DT::datatable(
+            filter = "top"
+          )
+      } else {
+        print("link clicking")
+        r$df[r$cols] %>%
+          LimpiaR::limpiar_link_click(!!rlang::sym(r$url_var)) %>% 
+          DT::datatable(
+            filter = "top",
+            escape = FALSE
+            # extensions = c("Buttons"),
+            # options = list(
+            #   select = list(maxOptions = 2000),
+            #   dom = 'Bfrtip',
+            #   buttons = c("copy", "csv", "excel", "pdf")
+            # )
+          )
+      }
     })
     
     
